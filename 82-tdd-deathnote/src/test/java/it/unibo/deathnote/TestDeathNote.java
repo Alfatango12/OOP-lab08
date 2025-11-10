f1bb34b907805cd9a7ac07d8ec43cba116e4570e
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-10 12:13:18 +0100

    Fixed beforeAllmethod in tests

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,30 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
+class TestDeathNote {
+    private static final int RULE_0 = 0;
+    private DeathNoteImpl dn;
+
+    @BeforeAll
+    /*
+     * Function that create a new object {@link DeathNoteImpl}
+     * that will be used for all the tests
+     */
+    public void setUp() {
+        this.dn = new DeathNoteImpl();
+    }
+
+    @Test
+    /*
+     * 
+     */
+    public void testRuleZero() {
+        try {
+            dn.getRule(RULE_0);
+        }
+    }
 }
