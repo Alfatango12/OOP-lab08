89a4098a5141c587dc9898f097a7ff13516a20b2
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-10 10:17:57 +0100

    Added the new JPanel in the center of the original canvas

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -1,5 +1,6 @@
 package it.unibo.mvc;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -32,6 +33,7 @@ public class MiniGUI {
         final JPanel canvas = new JPanel();
         final JPanel centerPanel = new JPanel();
         canvas.setLayout(new BorderLayout());
+        centerPanel.setLayout(new BoxLayout(centerPanel, PROPORTION));
         final JButton write = new JButton("Print a random number on standard output");
         canvas.add(write, BorderLayout.CENTER);
         frame.setContentPane(canvas);
