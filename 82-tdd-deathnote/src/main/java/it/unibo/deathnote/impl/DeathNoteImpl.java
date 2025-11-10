4e7162ea04dd41fc494480e16a703e71e8551c0d
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-10 11:45:09 +0100

    Added inheritDoc to all overrided methods

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,68 @@
+package it.unibo.deathnote.impl;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImpl implements DeathNote {
+
+    @Override
+    /*
+     * {@inheritDoc}
+     */
+    public String getRule(int ruleNumber) {
+        if (ruleNumber <= 0) {
+            throw new IllegalArgumentException("The argument must be a number greater than 0");
+        }
+
+        else {
+            return RULES.get(ruleNumber);
+        }
+    }
+
+    @Override
+    /*
+     * {@inheritDoc}
+     */
+    public void writeName(String name) {
+        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+    }
+
+    @Override
+    /*
+     * {@inheritDoc}
+     */
+    public boolean writeDeathCause(String cause) {
+        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+    }
+
+    @Override
+    /*
+     * {@inheritDoc}
+     */
+    public boolean writeDetails(String details) {
+        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+    }
+
+    @Override
+    /*
+     * {@inheritDoc}
+     */
+    public String getDeathCause(String name) {
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+    }
+
+    @Override
+    /*
+     * {@inheritDoc}
+     */
+    public String getDeathDetails(String name) {
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+    }
+
+    @Override
+    /*
+     * {@inheritDoc}
+     */
+    public boolean isNameWritten(String name) {
+        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+    }
+}
