9346497f35fc5687dcd730257600a7d18f9e7a46
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-10 13:00:29 +0100

    Implemented the first set of tests

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,15 +1,23 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.unibo.deathnote.api.DeathNote;
 import it.unibo.deathnote.impl.DeathNoteImpl;
 
 class TestDeathNote {
     private static final int RULE_0 = 0;
+    private static final int NEG_RULE = -1;
+    private static final String EMPY_STRING = "";
     private DeathNoteImpl dn;
 
-    @BeforeAll
+    @BeforeEach
     /*
      * Function that create a new object {@link DeathNoteImpl}
      * that will be used for all the tests
@@ -18,13 +26,44 @@ class TestDeathNote {
         this.dn = new DeathNoteImpl();
     }
 
+    /**
+     * 
+     */
     @Test
+    void testGetRuleZero() {
+        try {
+            this.dn.getRule(RULE_0);
+        } catch (IllegalArgumentException e) {
+            assertNotEquals(e.getMessage(), EMPY_STRING);
+        } 
+    }
+
+    /**
+     * 
+     */
+    @Test
+    void testGetNegativeRule() {
+        try {
+            this.dn.getRule(NEG_RULE);
+        } catch (IllegalArgumentException e) {
+            assertNotEquals(e.getMessage(), EMPY_STRING);
+        }
+    }
+
+    /**
+     * 
+     */
+    @Test
+    void testNullRules() {
+        for (int i = 1; i <= DeathNote.RULES.size(); i++) {
+            String rule = dn.getRule(i);
+            assertNotNull(rule);
+            assertNotEquals(rule, EMPY_STRING);
+        }
+    }
+
     /*
      * 
      */
-    public void testRuleZero() {
-        try {
-            dn.getRule(RULE_0);
-        }
-    }
-}
+    
+} 
