e74b4b51f08a25142a6d5cc6921bedd525e59285
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-11 10:32:14 +0100

    Implemented test number 4

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,8 +1,10 @@
 package it.unibo.deathnote;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.EmptyStackException;
 
@@ -15,9 +17,12 @@ import it.unibo.deathnote.impl.DeathNoteImpl;
 class TestDeathNote {
     private static final int RULE_0 = 0;
     private static final int NEG_RULE = -1;
+    private static final long sleepTime = 100;
     private static final String EMPY_STRING = "";
     private static final String humanName = "Gianfranco Spiriponzi";
     private static final String humanNameB = "Gesualdo Stiripaldo";
+    private static final String deathCause = "Karting Accident";
+    private static final String defaultDeathCause = "Heart Attack";
     private DeathNoteImpl dn;
 
     @BeforeEach
@@ -73,6 +78,29 @@ class TestDeathNote {
             assertEquals(dn.isNameWritten(EMPY_STRING), false);
 
         } catch (final NullPointerException e) {
+            System.out.println("Correctly managed exception " + e.toString() + " with message: " + e.getMessage() + " With stack trace: " + e.getStackTrace());
+        }
+    }
+
+    /**
+     * 
+     * @throws InterruptedException for the {@link Thread.sleep} function
+     */
+    @Test
+    void testDeathCauseTiming() throws InterruptedException {
+        try {
+            dn.writeDeathCause(deathCause);
+            dn.writeName(humanName);
+            assertEquals(dn.getDeathCause(humanName), defaultDeathCause);
+            dn.writeName(humanNameB);
+            assertTrue(dn.writeDeathCause(deathCause));
+            assertEquals(dn.getDeathCause(humanNameB), deathCause);
+            Thread.sleep(sleepTime);
+            assertFalse(dn.writeDeathCause(defaultDeathCause));
+            assertEquals(dn.getDeathCause(humanNameB), deathCause);
+        } catch (final IllegalStateException e) {
+            System.out.println("Correctly managed exception " + e.toString() + " with message: " + e.getMessage() + " With stack trace: " + e.getStackTrace());
+        } catch (final IllegalArgumentException e) {
             System.out.println("Correctly managed exception " + e.toString() + " with message: " + e.getMessage() + " With stack trace: " + e.getStackTrace());
         }
     }
