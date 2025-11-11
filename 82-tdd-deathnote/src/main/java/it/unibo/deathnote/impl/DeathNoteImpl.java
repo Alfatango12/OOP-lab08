908af8b8ebe0efe0d5f2db431f6f5d0266f3efce
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-11 15:46:22 +0100

    Fixed a bunch of checkStyle errors

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -5,69 +5,67 @@ import java.util.Map;
 
 import it.unibo.deathnote.api.DeathNote;
 
-/*
- * Implementation of the interface {@link DeathNote}
+/**
+ * Implementation of the interface {@link DeathNote}.
  */
 public final class DeathNoteImpl implements DeathNote {
     private static final int RULE_OFFSET = 1;
-    private static long DEATH_MILLIS_TIME = 40;
-    private static long DETAILS_MILLIS_TIME = 6000 + DEATH_MILLIS_TIME;
-    private static String EMPTY_STRING = "";
+    private static final long DEATH_MILLIS_TIME = 40;
+    private static final long DETAILS_MILLIS_TIME = 6000 + DEATH_MILLIS_TIME;
+    private static final String EMPTY_STRING = "";
 
     private Map<String, Death> deathNote;
     private String lastNameInserted;
     private long timeInMillis;
-    
+
+    /**
+     * 
+     */
     public DeathNoteImpl() {
         this.deathNote = new HashMap<String, Death>();
         this.lastNameInserted = "";
         this.timeInMillis = 0;
     }
 
-    @Override
-    /*
+    /**
      * {@inheritDoc}
      */
+    @Override
     public String getRule(final int ruleNumber) {
         if (ruleNumber <= 0 || ruleNumber >= DeathNote.RULES.size() + RULE_OFFSET) {
-            throw new IllegalArgumentException("The argument must be a number greater than 0 and lower than the list size + " + RULE_OFFSET);
-        }
-
-        else {
+            final String message = "The argument must be a number greater than 0 and lower than the list size + " + RULE_OFFSET;
+            throw new IllegalArgumentException(message);
+        } else {
             return RULES.get(ruleNumber - RULE_OFFSET);
         }
     }
 
-    @Override
-    /*
+    /**
      * {@inheritDoc}
      */
+    @Override
     public void writeName(final String name) {
         if (name == null) {
             throw new IllegalArgumentException("No name present in the deathNote, or the name in input is NULL");
-        }
-
-        else if (name.equals(EMPTY_STRING)) {
+        } else if (name.equals(EMPTY_STRING)) {
             System.err.println("Cannot insert an empty string, non a valid name");
-        }
-
-        else {
+        } else {
             this.deathNote.put(name, new Death());
             this.lastNameInserted = name;
             this.timeInMillis = System.currentTimeMillis();
         }
-        
+
     }
 
-    @Override
-    /*
+    /**
      * {@inheritDoc}
      */
+    @Override
     public boolean writeDeathCause(final String cause) {
-        if (this.lastNameInserted == null || cause ==  null) {
+        if (this.lastNameInserted == null || cause == null) {
             throw new IllegalStateException("The are no names in deathNote, or the cause parameter is NULL");
         }
-        
+
         if (checkTime(DEATH_MILLIS_TIME)) {
             deathNote.get(this.lastNameInserted).setDeathCause(cause);
             this.timeInMillis = System.currentTimeMillis();
@@ -76,13 +74,14 @@ public final class DeathNoteImpl implements DeathNote {
         return false;
     }
 
-    @Override
-    /*
+    /**
      * {@inheritDoc}
      */
+    @Override
     public boolean writeDetails(final String details) {
         if (this.lastNameInserted == null || details == null) {
-            throw new IllegalArgumentException("There are no names in deathNote, or the cause parameter is NULL"); //Note: this can be transformed in a function
+            final String message = "There are no names in deathNote, or the cause parameter is NULL";
+            throw new IllegalArgumentException(message); //Note: this can be transformed in a function
         }
 
         if (checkTime(DETAILS_MILLIS_TIME)) {
@@ -92,10 +91,10 @@ public final class DeathNoteImpl implements DeathNote {
         return false;
     }
 
-    @Override
-    /*
+    /**
      * {@inheritDoc}
      */
+    @Override
     public String getDeathCause(final String name) {
         if (!isNameWritten(name)) {
             throw new IllegalArgumentException("The name is not present in the deathNote");
@@ -104,10 +103,10 @@ public final class DeathNoteImpl implements DeathNote {
         return deathNote.get(name).getDeathCause();
     }
 
-    @Override
-    /*
+    /**
      * {@inheritDoc}
      */
+    @Override
     public String getDeathDetails(final String name) {
         if (!isNameWritten(name)) {
             throw new IllegalArgumentException("The name is not present in the deathNote");
@@ -116,10 +115,10 @@ public final class DeathNoteImpl implements DeathNote {
         return deathNote.get(name).getDetails();
     }
 
-    @Override
-    /*
+    /**
      * {@inheritDoc}
      */
+    @Override
     public boolean isNameWritten(final String name) {
         return deathNote.containsKey(name);
     }
@@ -132,30 +131,47 @@ public final class DeathNoteImpl implements DeathNote {
         return false;
     }
 
+    /**
+     * 
+     */
     public class Death {
-        private static String DEFAULT_DEATH_CAUSE = "Heart Attack";
-        private static String EMPTY_STRING = "";
+        private static final String DEFAULT_DEATH_CAUSE = "Heart Attack";
 
         private String deathCause;
         private String deathDetails;
 
+        /**
+         * 
+         */
         public Death() {
-            deathCause = DEFAULT_DEATH_CAUSE;
-            deathDetails = EMPTY_STRING;
+            this.deathCause = DEFAULT_DEATH_CAUSE;
+            this.deathDetails = DeathNoteImpl.EMPTY_STRING;
         }
 
+        /**
+         * @return d
+         */
         public String getDeathCause() {
             return this.deathCause;
         }
 
+        /**
+         * @return d
+         */
         public String getDetails() {
             return this.deathDetails;
         }
 
+        /**
+         * @param d
+         */
         public void setDeathCause(final String cause) {
             this.deathCause = cause;
         }
 
+        /**
+         * @param d
+         */
         public void setDeathDetails(final String details) {
             this.deathDetails = details;
         }
