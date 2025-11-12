8860053e62bb6161a98eb95329edb209637569f1
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-12 12:54:18 +0100

    Implemented the new view in stdout and added to the main app. First point completed

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStandardOutputView.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStandardOutputView.java
@@ -4,24 +4,30 @@ import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.api.DrawResult;
 
-public class DrawNumberStandardOutputView implements DrawNumberView{
+public class DrawNumberStandardOutputView implements DrawNumberView {
+    private DrawNumberController controller;
 
     @Override
     public void setController(DrawNumberController observer) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setController'");
+        this.controller = observer;
     }
 
     @Override
     public void start() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
+        
     }
 
     @Override
     public void result(DrawResult res) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'result'");
+        if (res == DrawResult.YOURS_HIGH || res == DrawResult.YOURS_LOW) {
+            System.out.println(res.getDescription());
+            return;
+        } else if (res == DrawResult.YOU_WON) {
+            System.out.println(res.getDescription() + " a new game starts!");
+        } else {
+            System.out.println(res.getDescription() + ": Lost!");
+        }
+        this.controller.resetGame();
     }
 
 }
